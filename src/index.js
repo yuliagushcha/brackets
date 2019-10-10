38819329d4c604e5ee4f90f371419f7b3b510ff1
yuliagushcha <Yulia_Gushcha@mail.ru>
2019-10-10 20:51:01 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = str.split('');
+    let arr = [];
+    let openBracket = ['{', '(', '[', '|', '1', '3', '5', '7', '8']; // задаем открывающие скобки
+    let closeBracket = ['}', ')', ']', '|', '2', '4', '6', '7', '8']; // задаем закрывающие скобки
+    let openBracketIndex;
+    let closeBracketIndex;
+
+    for (let i = 0, len = brackets.length; i < len; i++) {  // проверяем каждую скобку
+       openBracketIndex = openBracket.indexOf(brackets[i]);
+       if (openBracketIndex !== -1) { // находим открывающую скобку
+           arr.push(openBracketIndex); // помещаем ее в массив
+           continue;
+       }
+       console.log(arr);
+
+       closeBracketIndex = closeBracket.indexOf(brackets[i]);
+       if (closeBracketIndex !== -1) { // находим закрывающую скобку
+           openBracketIndex = arr.pop(); // так как для нее пару нашли, удаляем ее из массива
+           if (closeBracketIndex !== openBracketIndex)
+            {
+               return false;
+           }
+       }
+    }
+
+    if (arr.length !== 0) {  // делаем проверку на оставшиеся в строке скобки
+        return false;
+    }
+
+    return true;
 }
